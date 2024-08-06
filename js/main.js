7658e55957df6e162b04ca9d9a4ef84197ea0239
seongikx <tjddlr3512@pusan.ac.kr>
2024-08-06 21:03:33 +0900

    feat: mainpage

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.getElementById("start-btn").addEventListener("click", function () {
   var name = document.getElementById("name-input").value.trim();
   if (name) {
-    alert(`안녕하세요, ${name}님! 건강유형 테스트를 시작합니다.`);
+    alert(`${name}님의 건강유형 테스트를 시작합니다!`);
     window.location.href = "Pages/quiz.html";
   } else {
     alert("이름을 입력해 주세요.");
